a919c3595ab0692ac7d917ee6b8a5c8ea1321298
Cesar <cesarnsingi@hotmail.com>
2025-02-28 13:46:23 +0200

    import ui material

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,6 +2,14 @@ import React from 'react';
 import Logo from "../Assets/Logo.svg"
 import { BsCart2 } from "react-icons/bs";
 import { HiOutlineBars3 } from "react-icons/hi2";
+import { 
+    Box,
+    Drawer,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+} from "@mui/material";
 
 
 const Navbar = () => {
