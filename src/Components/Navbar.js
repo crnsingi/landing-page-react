82b48f5ad9417f2828d9666b0e4f172f0d63a51f
Cesar <cesarnsingi@hotmail.com>
2025-02-28 13:44:11 +0200

    import houtline bars 3

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Logo from "../Assets/Logo.svg"
 import { BsCart2 } from "react-icons/bs";
+import { HiOutlineBars3 } from "react-icons/hi2";
 
 
 const Navbar = () => {
